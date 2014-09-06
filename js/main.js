ac6d2d6045799692d30d12926df48c2e99532b58
Denzel Pressey <pressey90@gmail.com>
2014-09-06 19:07:37 -0400

    create update second functions, unexpected identifier

--- js/main.js
+++ js/main.js
@@ -1,46 +1,64 @@
-(function(){
+(function() {
 
-	// Create Datetime objects for each city
-	var crtSydy = new Date(2014, 8, 6, 4, 0, 0);
-	var crtTky  = new Date(2014, 8, 6, 3, 0, 0);
-	var crtLndn = new Date(2014, 8, 6, 7, 0, 0);
-	var crtNwyrk  = new Date(2014, 8, 6, 2, 0, 0);
-
-
-	//  Insert times into the DOM
-	var insrtDom = function (){
-
-		// set Sydney's time into the paragraph tag with it's css id
-		var sydyTime = document.getElementById("sydney-time");
-		sydyTime.innerHTML = crtSydy.getHours() + ":" + crtSydy.getMinutes() + ":" + crtSydy.getSeconds();
-		
-		// set Tokyo's time into the paragraph tag with it's css id
-		var tkyTime = document.getElementById("tokyo-time");
-		tkyTime.innerHTML = crtTky.getHours() + ":" + crtTky.getMinutes() + ":" + crtTky.getSeconds();
-
-		// set London's time into the paragraph tag with it's css id
-		var lndnTime = document.getElementById("london-time");
-		lndnTime.innerHTML = crtLndn.getHours() + ":" + crtLndn.getMinutes() + ":" + crtLndn.getSeconds();
-
-		// set New York City's time into the paragraph tag with it's css id
-		var nwyrkTime = document.getElementById("nyc-time");
-		nwyrkTime.innerHTML = crtNwyrk.getHours() + ":" + crtNwyrk.getMinutes() + ":" + crtNwyrk.getSeconds();
-
-	}; insrtDom();
+        // Create Datetime objects for each city
+        var crtSydy = new Date(2014, 8, 6, 4, 0, 0);
+        var crtTky = new Date(2014, 8, 6, 3, 0, 0);
+        var crtLndn = new Date(2014, 8, 6, 7, 0, 0);
+        var crtNwyrk = new Date(2014, 8, 6, 2, 0, 0);
 
 
+        //  Insert times into the DOM
+        var insrtDom = function() {
 
-	//  Update seconds
+            // set Sydney's time into the paragraph tag with it's css id
+            var sydyTime = document.getElementById("sydney-time");
+            sydyTime.innerHTML = crtSydy.getHours() + ":" + crtSydy.getMinutes() + ":" + crtSydy.getSeconds();
+
+            // set Tokyo's time into the paragraph tag with it's css id
+            var tkyTime = document.getElementById("tokyo-time");
+            tkyTime.innerHTML = crtTky.getHours() + ":" + crtTky.getMinutes() + ":" + crtTky.getSeconds();
+
+            // set London's time into the paragraph tag with it's css id
+            var lndnTime = document.getElementById("london-time");
+            lndnTime.innerHTML = crtLndn.getHours() + ":" + crtLndn.getMinutes() + ":" + crtLndn.getSeconds();
+
+            // set New York City's time into the paragraph tag with it's css id
+            var nwyrkTime = document.getElementById("nyc-time");
+            nwyrkTime.innerHTML = crtNwyrk.getHours() + ":" + crtNwyrk.getMinutes() + ":" + crtNwyrk.getSeconds();
+
+        };
+        insrtDom();
+
+
+        //  Update seconds
+        function updtScnds(sydney) {
+
+            var crntScnds = sydney.getSeconds();
+            var newScnd = sydney.setSeconds(crntScnds + 1);
+            if (newScnd > 59) {
+                var newScnd = 0;
+                document.getElementById("sydney-time").innerHTML = crtSydy.getHours() + ":" + crtSydy.getMinutes() + ":" + newScnd;
+            } else {
+                document.getElementById("sydney-time").innerHTML = crtSydy.getHours() + ":" + crtSydy.getMinutes() + ":" + newScnd;
+            }
+        }
+
+        bouncer();
+    }
+    updtScnds(crtSydy);
+
+    // callback for updating time
+    var bouncer = function() {
+        window.setTimeout(updtScnds, 1000);
+    }; bouncer();
 
 
 
 
-
-	// Update minutes
-
+    // Update minutes
 
 
 
 
-	// Update hours
-}) ();
+    // Update hours */
+})();
