3123e8340e12dfd5cc88041240bfcccd8668a91a
Denzel Pressey <pressey90@gmail.com>
2014-09-06 14:33:07 -0400

    create app

---
+++ js/main.js
@@ -0,0 +1,36 @@
+(function(){
+
+	// Create Datetime objects for each city
+	var crtSydy = function(){
+		var currentDate = new Date(2014, 8, 6, 4, 0, 0);
+	};
+
+	var crtTky = function(){
+		var currentDate = new Date(2014, 8, 6, 3, 0, 0);
+	};
+	var crtLndn = function(){
+		var currentDate = new Date(2014, 8, 6, 7, 0, 0);
+	};
+	var crtNwyrk = function(){
+		var currentDate = new Date(2014, 8, 6, 2, 22, 0);
+	};
+
+	//  Insert times into the DOM
+
+
+
+
+	//  Update seconds
+
+
+
+
+
+	// Update minutes
+
+
+
+
+
+	// Update hours
+}) ();
